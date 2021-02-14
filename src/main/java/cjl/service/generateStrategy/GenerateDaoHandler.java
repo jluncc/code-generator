6936decaab8f87dab6603bc27a2526d75a15ce5b
jluncc <cjljinglun@163.com>
2021-02-14 22:44:24 +0800

    feat: 优化dao文件生成逻辑

--- src/main/java/cjl/service/generateStrategy/GenerateDaoHandler.java
+++ src/main/java/cjl/service/generateStrategy/GenerateDaoHandler.java
@@ -19,64 +19,76 @@ import java.util.Map;
 public class GenerateDaoHandler implements GenerateStrategyHandler {
     @Override
     public void generateFile(DbInfo dbInfo, GeneratorInfo generatorInfo, List<ColumnInfo> columnInfos, PluginInfo pluginInfo, Map<Object, Object> params) {
-        String fileName = String.format("%sMapper.java", StrUtil.line2Hump(dbInfo.getTableName(), true));
+        String orm = generatorInfo.getOrm();
+        // 如果是mybatis，还要生成mapper文件 TODO mybatis待优化生成注解文件
+        if (BizConstant.ORM.MyBatis.equalsIgnoreCase(orm)) {
+            String fileName = String.format("%sMapper.java", StrUtil.line2Hump(dbInfo.getTableName(), true));
+            handleMybatisGenerate(dbInfo, generatorInfo, columnInfos, fileName);
+        }
+        if (BizConstant.ORM.JPA.equals(orm)) {
+            handleJPAGenerate(dbInfo, generatorInfo, columnInfos);
+        }
+    }
+
+    private void handleMybatisGenerate(DbInfo dbInfo, GeneratorInfo generatorInfo, List<ColumnInfo> columnInfos, String fileName) {
         String packageBaseLocation = generatorInfo.getPackageBaseLocation();
         String packageBaseName = generatorInfo.getPackageBaseName();
         ModuleInfo generatorDao = generatorInfo.getGeneratorDao();
-
-        String orm = generatorInfo.getOrm();
         String entityPackageName = generatorInfo.getEntityPackageName();
 
-        // 如果是mybatis，还要生成mapper文件 TODO mybatis待优化生成注解文件
-        if (BizConstant.ORM.MyBatis.equalsIgnoreCase(orm)) {
-            String filePath = String.format("%s/mapper", packageBaseLocation);
-            String packageName = String.format("%s,mapper", packageBaseName);
-            if (StringUtils.isNotEmpty(generatorDao.getDetailPackageName())) {
-                filePath = packageBaseLocation + generatorDao.getDetailPackageName();
-                packageName = packageBaseName + generatorDao.getDetailPackageName().replaceAll("/", ".");
-            }
-            generatorInfo.setDaoPackageName(String.format("%s.%sMapper", packageName, StrUtil.line2Hump(dbInfo.getTableName(), true)));
-
-            String finalDaoFilePath = filePath + fileName;
-            if (!filePath.endsWith("/")) {
-                finalDaoFilePath = String.format("%s/%s", filePath, fileName);
-            }
-            LogUtil.SYS.info("生成dao文件的路径为：{}", finalDaoFilePath);
-            File mapperFile = new File(finalDaoFilePath);
-            if (!FileUtil.mkdirs(mapperFile.getParentFile())) return;
-
-            Map<String, Object> dataMap = new HashMap<>();
-            dataMap.put("columns", columnInfos);
-            dataMap.put("packageName", packageName);
-            dataMap.put("entityPackageName", entityPackageName);
-            CommonGenerateHandler.generatorFileByTemplate("Mapper.ftl", mapperFile, dataMap, dbInfo, generatorInfo);
-            LogUtil.SYS.info("生成dao文件完毕");
-            CommonGenerateHandler.generatorMapperXML(dataMap, generatorDao.getMapperXMLPath(), dbInfo.getTableName(), dbInfo, generatorInfo);
+        String filePath = String.format("%s/mapper", packageBaseLocation);
+        String packageName = String.format("%s,mapper", packageBaseName);
+        if (StringUtils.isNotEmpty(generatorDao.getDetailPackageName())) {
+            filePath = packageBaseLocation + generatorDao.getDetailPackageName();
+            packageName = packageBaseName + generatorDao.getDetailPackageName().replaceAll("/", ".");
         }
-        if (BizConstant.ORM.JPA.equals(orm)) {
-            fileName = String.format("%sRepository.java", StrUtil.line2Hump(dbInfo.getTableName(), true));
-            String filePath = String.format("%s/repository", packageBaseLocation);
-            String packageName = String.format("%s,repository", packageBaseName);
-            if (StringUtils.isNotEmpty(generatorDao.getDetailPackageName())) {
-                filePath = packageBaseLocation + generatorDao.getDetailPackageName();
-                packageName = packageBaseName + generatorDao.getDetailPackageName().replaceAll("/", ".");
-            }
-            generatorInfo.setDaoPackageName(String.format("%s.%sMapper", packageName, StrUtil.line2Hump(dbInfo.getTableName(), true)));
+        generatorInfo.setDaoPackageName(String.format("%s.%sMapper", packageName, StrUtil.line2Hump(dbInfo.getTableName(), true)));
 
-            String finalDaoFilePath = filePath + fileName;
-            if (!filePath.endsWith("/")) {
-                finalDaoFilePath = String.format("%s/%s", filePath, fileName);
-            }
-            LogUtil.SYS.info("生成dao文件的路径为：{}", finalDaoFilePath);
-            File repositoryFile = new File(finalDaoFilePath);
-            if (!FileUtil.mkdirs(repositoryFile.getParentFile())) return;
-
-            Map<String, Object> dataMap = new HashMap<>();
-            dataMap.put("columns", columnInfos);
-            dataMap.put("packageName", packageName);
-            dataMap.put("entityPackageName", entityPackageName);
-            CommonGenerateHandler.generatorFileByTemplate("Repository.ftl", repositoryFile, dataMap, dbInfo, generatorInfo);
-            LogUtil.SYS.info("生成dao文件完毕");
+        String finalDaoFilePath = filePath + fileName;
+        if (!filePath.endsWith("/")) {
+            finalDaoFilePath = String.format("%s/%s", filePath, fileName);
         }
+        LogUtil.SYS.info("生成dao文件的路径为：{}", finalDaoFilePath);
+        File mapperFile = new File(finalDaoFilePath);
+        if (!FileUtil.mkdirs(mapperFile.getParentFile())) return;
+
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("columns", columnInfos);
+        dataMap.put("packageName", packageName);
+        dataMap.put("entityPackageName", entityPackageName);
+        CommonGenerateHandler.generatorFileByTemplate("Mapper.ftl", mapperFile, dataMap, dbInfo, generatorInfo);
+        LogUtil.SYS.info("生成dao文件完毕");
+        CommonGenerateHandler.generatorMapperXML(dataMap, generatorDao.getMapperXMLPath(), dbInfo.getTableName(), dbInfo, generatorInfo);
+    }
+
+    private void handleJPAGenerate(DbInfo dbInfo, GeneratorInfo generatorInfo, List<ColumnInfo> columnInfos) {
+        String packageBaseLocation = generatorInfo.getPackageBaseLocation();
+        String packageBaseName = generatorInfo.getPackageBaseName();
+        ModuleInfo generatorDao = generatorInfo.getGeneratorDao();
+        String entityPackageName = generatorInfo.getEntityPackageName();
+
+        String fileName = String.format("%sRepository.java", StrUtil.line2Hump(dbInfo.getTableName(), true));
+        String filePath = String.format("%s/repository", packageBaseLocation);
+        String packageName = String.format("%s,repository", packageBaseName);
+        if (StringUtils.isNotEmpty(generatorDao.getDetailPackageName())) {
+            filePath = packageBaseLocation + generatorDao.getDetailPackageName();
+            packageName = packageBaseName + generatorDao.getDetailPackageName().replaceAll("/", ".");
+        }
+        generatorInfo.setDaoPackageName(String.format("%s.%sMapper", packageName, StrUtil.line2Hump(dbInfo.getTableName(), true)));
+
+        String finalDaoFilePath = filePath + fileName;
+        if (!filePath.endsWith("/")) {
+            finalDaoFilePath = String.format("%s/%s", filePath, fileName);
+        }
+        LogUtil.SYS.info("生成dao文件的路径为：{}", finalDaoFilePath);
+        File repositoryFile = new File(finalDaoFilePath);
+        if (!FileUtil.mkdirs(repositoryFile.getParentFile())) return;
+
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("columns", columnInfos);
+        dataMap.put("packageName", packageName);
+        dataMap.put("entityPackageName", entityPackageName);
+        CommonGenerateHandler.generatorFileByTemplate("Repository.ftl", repositoryFile, dataMap, dbInfo, generatorInfo);
+        LogUtil.SYS.info("生成dao文件完毕");
     }
 }
